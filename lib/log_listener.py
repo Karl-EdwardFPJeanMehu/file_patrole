6a5972275412b8c745e566a22d9c67410da0fa3d
Karl-Edward F P Jean-Mehu <karlej99@yahoo.com>
2023-10-22 23:25:24 -0400

    Refactor: Decouple event using observer pattern

---
+++ lib/log_listener.py
@@ -0,0 +1,32 @@
+from os import path
+from .logger import log
+from .event import subscribe
+from termcolor import colored
+
+log_location = "log.txt"
+
+def handle_file_added_event(data):
+    file_path = data["file_path"]
+    file_hash= data["file_hash"]
+    file_name = path.basename(data["file_path"])
+    description = "The file, {file_name}, found at {file_path} has been added by user, pete"
+
+    print(colored(f"The file {file_name} has been added!\r\n", 'green'))
+
+    # Write event to log file
+    log(log_location, file_path, "pete", file_hash, "received_hashoaijsodifjoidje", description)
+
+def handle_file_modified_event(data):
+    file_path = data["file_path"]
+    file_hash= data["file_hash"]
+    file_name = path.basename(data["file_path"])
+    description = "The file, {file_name}, found at {file_path} has been modified by user, pete"
+
+    print(colored(f"The file {file_name} has been modified!\r\n", 'yellow'))
+
+    # Write event to log file
+    log(log_location, file_path, "pete", file_hash, "received_hashoaijsodifjoidje", description)
+
+def setup_log_event_handlers():
+    subscribe("file_added", handle_file_added_event)
+    subscribe("file_modified", handle_file_modified_event)

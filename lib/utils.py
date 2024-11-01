721ffe4d848e9ea0ca2b84388bd8db8054a726de
Karl-Edward FP Jean-Mehu <karlej99@yahoo.com>
2024-11-01 16:30:09 -0400

    Feat: Added credit banner

--- lib/utils.py
+++ lib/utils.py
@@ -12,3 +12,6 @@ def normalize_path(path):
 
     normalized_path = os.path.normpath(path)
     return normalized_path.replace('//', '/')
+
+def banner():
+    print(f"{"=" * 80}\r\n FilePatrole v1.0\r\n Copyright (c) 2023, Karl-Edward F. P. Jean-Mehu <kwebdever@protonmail.com>\r\n licensed under MIT\r\n{"=" * 80}")

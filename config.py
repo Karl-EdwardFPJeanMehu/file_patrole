9c29a791530c696c987004d88458bb5c3f2b4a8a
Karl-Edward FP Jean-Mehu <karlej99@yahoo.com>
2024-11-02 22:32:35 -0400

    Feat: Added central config file

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+
+class Config:
+    # Creates a new config object
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Config, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        # config_dir = os.path.dirname(os.path.abspath(__file__))
+        # root_dir = os.path.dirname(__file__)
+
+        self.config = {
+            "BASELINE_PATH": os.environ.get("PT_BASELINE_PATH", "./baseline"),
+            "MONITOR_DIR": os.environ.get("PT_MONITOR_DIR", "./"),
+        }
+
+    def get(self, key):
+        # Returns the value of the specified key
+        # if not it throws an error
+        if key not in self.config:
+            if key not in os.environ:
+                print(f"configs: {self.config}")
+                raise KeyError(f"Config key,'{key}', not found.")
+
+            else:
+                self.config[key] = os.environ.get(key)
+
+        return self.config.get(key)
+
+    def set(self, key, value):
+        print(f"Setting config '{key}' to '{value}'")
+        os.environ[key] = value
+        self.config[key] = value

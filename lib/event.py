a130a860d513b73d7c37a41aa24cfb2b00039916
Karl-Edward FP Jean-Mehu <karlej99@yahoo.com>
2025-02-14 21:48:59 -0500

    Fix: Test for membership anti-pattern

--- lib/event.py
+++ lib/event.py
@@ -2,14 +2,13 @@ subscribers = dict()
 
 def subscribe(event_type: str, fn):
     event_type = event_type.lower()
-    if not event_type in subscribers:
+    if event_type not in subscribers:
         subscribers[event_type] = []
     subscribers[event_type].append(fn)
 
 def post_event(event_type: str, data):
     event_type = event_type.lower()
-    if not event_type in subscribers:
+    if event_type not in subscribers:
         return
     for fn in subscribers[event_type]:
         fn(data)
-
